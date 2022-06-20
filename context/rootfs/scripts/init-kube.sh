afdf5ce15394e4c1b8eaa04aa5f66504e8f514c8
wangfei <wf01031309@alibaba-inc.com>
2022-06-20 10:34:43 +0800

    Format script file

--- context/rootfs/scripts/init-kube.sh
+++ context/rootfs/scripts/init-kube.sh
@@ -9,43 +9,43 @@ modprobe -- br_netfilter
 ## version_ge 5.4 4.19 true ;
 ## version_ge 3.10 4.19 false ;
 
-version_ge(){
-    test "$(echo "$@" | tr ' ' '\n' | sort -rV | head -n 1)" == "$1"
+version_ge() {
+  test "$(echo "$@" | tr ' ' '\n' | sort -rV | head -n 1)" == "$1"
 }
 
-disable_selinux(){
-    if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
-        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-        setenforce 0
-    fi
+disable_selinux() {
+  if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+  fi
 }
 
 get_distribution() {
-	lsb_dist=""
-	# Every system that we officially support has /etc/os-release
-	if [ -r /etc/os-release ]; then
-		lsb_dist="$(. /etc/os-release && echo "$ID")"
-	fi
-	# Returning an empty string here should be alright since the
-	# case statements don't act unless you provide an actual value
-	echo "$lsb_dist"
+  lsb_dist=""
+  # Every system that we officially support has /etc/os-release
+  if [ -r /etc/os-release ]; then
+    lsb_dist="$(. /etc/os-release && echo "$ID")"
+  fi
+  # Returning an empty string here should be alright since the
+  # case statements don't act unless you provide an actual value
+  echo "$lsb_dist"
 }
 
 disable_firewalld() {
-  lsb_dist=$( get_distribution )
-	lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
-	case "$lsb_dist" in
-		ubuntu|deepin|debian|raspbian)
-			command -v ufw &> /dev/null && ufw disable
-		;;
-		centos|rhel|ol|sles|kylin|neokylin)
-			systemctl stop firewalld && systemctl disable firewalld
-		;;
-		*)
-		 	systemctl stop firewalld && systemctl disable firewalld
-			echo "unknown system, use default to stop firewalld"
-		;;
-	esac
+  lsb_dist=$(get_distribution)
+  lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+  case "$lsb_dist" in
+  ubuntu | deepin | debian | raspbian)
+    command -v ufw &>/dev/null && ufw disable
+    ;;
+  centos | rhel | ol | sles | kylin | neokylin)
+    systemctl stop firewalld && systemctl disable firewalld
+    ;;
+  *)
+    systemctl stop firewalld && systemctl disable firewalld
+    echo "unknown system, use default to stop firewalld"
+    ;;
+  esac
 }
 
 kernel_version=$(uname -r | cut -d- -f1)
@@ -55,7 +55,7 @@ else
   modprobe -- nf_conntrack_ipv4
 fi
 
-cat <<EOF >  /etc/sysctl.d/k8s.conf
+cat <<EOF >/etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.conf.all.rp_filter=0
